bfd70478215e4a7441410eab70ccf1a32eb12c7c
aestheticc <2952258-aestheticc@users.noreply.replit.com>
2023-09-04 04:00:51 +0000

    ep1

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ class PokeSpider(scrapy.Spider):
 
   def parse(self, response):
       linhas = response.css('table#pokedex > tbody > tr')
-      #for linha in linhas:
-      linha = linhas[20]
-      link = linha.css("td:nth-child(2) > a::attr(href)")
-      yield response.follow(link.get(), self.parser_pokemon)
+      for linha in linhas:
+      #linha = linhas[4]
+        link = linha.css("td:nth-child(2) > a::attr(href)")
+        yield response.follow(link.get(), self.parser_pokemon)
   
   def parser_pokemon(self,response):
     nome = response.css('main#main > h1::text')
@@ -22,8 +22,8 @@ class PokeSpider(scrapy.Spider):
     evolucoes = response.css('div.infocard-list-evo > div:nth-child(-n + 5) > span.infocard-lg-data.text-muted > a::text')
     evolucoesnumero = response.css('#main > div.infocard-list-evo > div:nth-child(-n + 5) > span.infocard-lg-data.text-muted > small:nth-child(-n + 5)::text')
     evolucoeslink = response.css('#main > div.infocard-list-evo > div:nth-child(-n + 5) > span.infocard-lg-data.text-muted > a::attr(href)')
-    habilidadenome = response.css('#tab-basic-16 > div:nth-child(1) > div:nth-child(2) > table > tbody > tr:nth-child(6) > td > span:nth-child(-n+5) > a::text')
-    habilidadelink = response.css('#tab-basic-16 > div:nth-child(1) > div:nth-child(2) > table > tbody > tr:nth-child(6) > td > span:nth-child(-n+5) > a::attr(href)')
+    habilidadenome = response.css('div:nth-child(1) > div:nth-child(2) > table > tbody > tr:nth-child(6) > td > span:nth-child(-n + 5) > a::text')
+    habilidadelink = response.css('div:nth-child(1) > div:nth-child(2) > table > tbody > tr:nth-child(6) > td > span:nth-child(-n + 5) > a::attr(href)')
 
     yield{"nome": nome.get(),"numero":numero.get(),"peso": peso.get(), "altura":altura.get(), "tipos:": tipo.get(), "tipo2": tipo2.get(), "URL:": urlpag,"Evolucoes Numero:": evolucoesnumero.getall() ,"Evolucoes: ": evolucoes.getall(), "Evolucoes link:": evolucoeslink.getall(), "Habilidade nome:": habilidadenome.getall(), "Habilidade Link:": habilidadelink.getall()}
 
